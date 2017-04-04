91b65719f2abf47a4b401172d77cf86f4ef3fcc2
pigff <androiddevzf@163.com>
2017-04-04 18:15:38 +0800

    首页有百分比布局替换

--- app/src/main/java/com/androiddev/zf/firstcodestudy/MainActivity.java
+++ app/src/main/java/com/androiddev/zf/firstcodestudy/MainActivity.java
@@ -6,16 +6,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn01;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn02;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn03;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn04;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn05;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn06;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn07;
+    /**
+     * 隐式跳转
+     */
+    private Button mBtn08;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.btn_01);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        initView();
+
+    }
+
+    private void initView() {
+        mBtn01 = (Button) findViewById(R.id.btn_01);
+        mBtn01.setOnClickListener(this);
+        mBtn02 = (Button) findViewById(R.id.btn_02);
+        mBtn02.setOnClickListener(this);
+        mBtn03 = (Button) findViewById(R.id.btn_03);
+        mBtn03.setOnClickListener(this);
+        mBtn04 = (Button) findViewById(R.id.btn_04);
+        mBtn04.setOnClickListener(this);
+        mBtn05 = (Button) findViewById(R.id.btn_05);
+        mBtn05.setOnClickListener(this);
+        mBtn06 = (Button) findViewById(R.id.btn_06);
+        mBtn06.setOnClickListener(this);
+        mBtn07 = (Button) findViewById(R.id.btn_07);
+        mBtn07.setOnClickListener(this);
+        mBtn08 = (Button) findViewById(R.id.btn_08);
+        mBtn08.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_01:
                 //隐式启动Activity
 //                Intent intent = new Intent("ACTION_START");
 //                startActivity(intent);
@@ -28,8 +84,21 @@ public class MainActivity extends BaseActivity {
                 Intent intent = new Intent();
                 intent.setData(Uri.parse("http://www.baidu.com"));
                 startActivity(intent);
-            }
-        });
-
+                break;
+            case R.id.btn_02:
+                break;
+            case R.id.btn_03:
+                break;
+            case R.id.btn_04:
+                break;
+            case R.id.btn_05:
+                break;
+            case R.id.btn_06:
+                break;
+            case R.id.btn_07:
+                break;
+            case R.id.btn_08:
+                break;
+        }
     }
 }
